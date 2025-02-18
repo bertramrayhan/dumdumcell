963a724728e51ab6d8948842d657b3065ac038f9
bertramrayhan <bertramrayhan1357@gmail.com>
2025-02-18 13:41:19 +0700

    menambahkann fitur logout dan menampilkan data karyawan

--- src/karyawan/halamanProfil/HalamanProfilKController.java
+++ src/karyawan/halamanProfil/HalamanProfilKController.java
@@ -1,14 +1,101 @@
 package karyawan.halamanProfil;
 
+import java.io.IOException;
 import java.net.URL;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import karyawan.halamanProfil.logout.LogoutController;
+import main.DumdumKasir;
+import main.Koneksi;
+import main.Session;
 
 public class HalamanProfilKController implements Initializable {
 
+    @FXML Button btnLogout, btnGantiPassword;
+    @FXML Label lblNama, lblAlamat, lblNomorHP, lblUsername;
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        
+        isiDataKaryawan();
     }    
     
+    @FXML
+    private void logout(){
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/karyawan/halamanProfil/logout/logout.fxml"));
+            Parent root = loader.load();
+
+            Stage logoutStage = new Stage();
+            logoutStage.initModality(Modality.APPLICATION_MODAL);
+            logoutStage.initStyle(StageStyle.UNDECORATED);
+            logoutStage.centerOnScreen();
+            logoutStage.setScene(new Scene(root));
+
+            LogoutController controller = loader.getController();
+            controller.setDialogStage(logoutStage);
+            
+            logoutStage.showAndWait(); // Tunggu user klik "Iya" atau "Tidak"
+
+            if (controller.isLogoutConfirmed()) {
+                switchToLogin(); // Logout ke halaman login
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    private void switchToLogin() {
+        try {
+            Stage loginStage = new Stage();
+            Parent root = FXMLLoader.load(DumdumKasir.class.getResource(Session.getPathHalamanLogin()));
+
+            Scene scene = new Scene(root);
+            loginStage.setScene(scene);
+            loginStage.setResizable(false);
+            loginStage.centerOnScreen();
+            loginStage.show();
+
+            DumdumKasir.halamanUtama.close(); // Tutup halaman utama
+            DumdumKasir.halamanUtama = loginStage; // Simpan halaman login sebagai utama
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    private void isiDataKaryawan(){
+        try {
+            String query = "SELECT nama, alamat, nomor_telepon, username FROM admin WHERE id_admin=?";
+            PreparedStatement statement = Koneksi.getCon().prepareStatement(query);
+            statement.setString(1, Session.getIdAdmin());
+            
+            ResultSet result = statement.executeQuery();
+            System.out.println(Session.getIdAdmin());
+            if(result.next()){
+                lblNama.setText(result.getString("nama"));
+                lblAlamat.setText(result.getString("alamat"));
+                lblNomorHP.setText(result.getString("nomor_telepon"));
+                lblUsername.setText(result.getString("username"));
+            }else{
+                System.out.println("tidak ketemu");
+            }
+            
+            result.close();
+            statement.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
