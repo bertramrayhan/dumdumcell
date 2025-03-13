bb48cc58471f1a4b7687b224a35f2f44f3a729b9
bertramrayhan <bertramrayhan1357@gmail.com>
2025-03-13 09:47:37 +0700

    desain halaman saldo selesai

---
+++ src/karyawan/halamanSaldo/HalamanSaldoKController.java
@@ -0,0 +1,14 @@
+package karyawan.halamanSaldo;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+public class HalamanSaldoKController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
+    }    
+    
+}
